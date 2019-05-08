dbfdf52c2291da2218d16b65c366791e5d7717ca
Simoncjh <865167122@qq.com>
2019-05-08 17:23:51 +0200

    sql emprunter

--- git_code/reserve_search_action.php
+++ git_code/reserve_search_action.php
@@ -17,16 +17,17 @@ $sql = "SELECT aeroport, p.label, prix_du_jour, adresse FROM parking p, "
         . " p.nombre_max > a.num) OR (p.label NOT IN (a.label))";
 }
 else{
-    $sql = "SELECT aeroport, p.label, adresse,p.n_plaque FROM parking p, "
-        . "(SELECT label_du_parking as label FROM gare g WHERE "
+    $sql = "SELECT aeroport, p.label, adresse FROM parking p, "
+        . "(SELECT label_du_parking as label,n_plaque FROM gare g WHERE "
        . "unix_timestamp(g.date_debut) < unix_timestamp('" .$info["time_debut"] ."') OR "
 //       . "unix_timestamp(g.date_debut)< unix_timestamp('05/08/2019 00:00:00') OR "
         . "unix_timestamp(g.date_fin) > unix_timestamp('" .$info["time_fin"]."'))a WHERE "
 //        . "unix_timestamp(g.date_fin)> unix_timestamp('05/09/2019 00:00:00'))a WHERE"
-        . "(p.aeroport ='" .$info["aeroport"] ."'AND"
-//        . " p.aeroport ='CDG' AND"
-        . " p.n_plaque NOT IN (SELECT e.n_plaque FROM emprunte e"
-        . "WHERE unix_timestamp(e.date_fin) > unix_timestamp('" .$info["time_debut"] ."'))";
+        . "p.aeroport ='" .$info["aeroport"] ."' AND "
+        . " p.label = a.label AND "
+        . " a.n_plaque NOT IN (SELECT e.n_plaque FROM emprunte e "
+        . "WHERE unix_timestamp(e.date_fin) > unix_timestamp('" .$info["time_fin"] ."') OR  "
+        . "unix_timestamp(e.date_debut) < unix_timestamp('" .$info["time_debut"] ."'))";
        
 }
 
