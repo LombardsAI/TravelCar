212857cdf7b29f563c81e3935827fb8f31c0de9f
Simoncjh <865167122@qq.com>
2019-06-01 18:54:34 +0200

    Merge branch 'simon'
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- git_code/app/controller/controllerReservation.php
+++ git_code/app/controller/controllerReservation.php
@@ -1,9 +1,10 @@
 <?php
 require_once ('../model/ModelReservation.php');
-
+require_once ('../model/ModelReservationVehicule.php');
 class controllerReservation
 {
     public static function reserverParking(){
+        $check = "parking";
         require ('../view/viewReserver_parking.php');
     }
 
@@ -32,5 +33,27 @@ class controllerReservation
         else{
             require ('../view/transition_fail.php');
         }
+   }
+   
+   public static function reserverVehicule(){
+        $check = "vehicule";
+         require ('../view/viewReserver_parking.php');
+   }
+   
+   public static function reserverVehiculeDetail($table){
+         $arr_debut = explode("/", $table["date_debut"]);
+        $arr_debut_1 = array($arr_debut[2],$arr_debut[0],$arr_debut[1]);
+        $arr_fin = explode("/", $table["date_fin"]);
+        $arr_fin_1 = array($arr_fin[2],$arr_fin[0],$arr_fin[1]);
+         $table["date_debut"] = implode("-", $arr_debut_1);
+         $table["date_fin"] = implode("-", $arr_fin_1);
+        $table["date_debut"].=" ".$table["time_debut"].":00";
+        $table["date_fin"].=" ".$table["time_fin"].":00";
+        $info = array();
+        $info["aeroport"] = $table["aeroport"];
+        $info["date_debut"] = $table["date_debut"];
+        $info["date_fin"] = $table["date_fin"];
+        $results = ModelReservationVehicule::reserver_search_vehicule($info);
+        require ('../view/viewReserver_vehicule_result.php');
    }
 }
